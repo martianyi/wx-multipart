d9cddc0975b6e148a288315703f99ff51ed974ba
Yi Kai <beliefs111@gmail.com>
2019-04-09 15:42:38 +0800

    make it compatible to miniprogram npm

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
-const path = require('path')
-const MinifyPlugin = require('babel-minify-webpack-plugin')
+const path = require('path');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
 module.exports = {
-	entry: './index.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'Multipart.min.js',
-		library: 'library',
-		libraryTarget: 'umd'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.m?js$/,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env']
-					}
-				}
-			}
-		]
-	},
-	mode: 'production',
-	plugins: [new MinifyPlugin()],
-	target: 'web',
-	optimization: {
-		minimize: false
-	}
-}
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'miniprogram_dist'),
+    filename: 'Multipart.min.js',
+    library: 'library',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  mode: 'production',
+  plugins: [new MinifyPlugin()],
+  target: 'web',
+  optimization: {
+    minimize: false
+  }
+};
